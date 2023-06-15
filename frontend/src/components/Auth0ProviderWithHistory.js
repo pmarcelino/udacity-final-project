1fbdfc28ba11f786f7f914178724d3d173f992dc
Pedro Marcelino <pmarcelino@gmail.com>
2023-06-15 03:43:26 +0100

    add support for fallback environment variables with .env.docker

--- frontend/src/components/Auth0ProviderWithHistory.js
+++ frontend/src/components/Auth0ProviderWithHistory.js
@@ -3,6 +3,13 @@ import { useNavigate } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const Auth0ProviderWithHistory = ({ children }) => {
+  const dotenv = require("dotenv");
+
+  // Load .env file if exists, otherwise load .env.docker
+  const result = dotenv.config({ path: ".env" });
+  if (result.error) {
+    dotenv.config({ path: ".env.docker" });
+  }
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
   const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
