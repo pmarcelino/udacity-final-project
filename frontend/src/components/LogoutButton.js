4d66bbdfa9720f7dfbe25732d970b534be4a87c5
Pedro Marcelino <pmarcelino@gmail.com>
2023-06-25 11:16:05 +0100

    change return link after logout

--- frontend/src/components/LogoutButton.js
+++ frontend/src/components/LogoutButton.js
@@ -3,12 +3,14 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
+  const returnLink = process.env.REACT_APP_AUTH0_LOGOUT_REDIRECT_URL;
+
   return (
     <button
       className="btn btn-danger btn-block"
       onClick={() =>
         logout({
-          returnTo: window.location.origin,
+          returnTo: returnLink,
         })
       }
     >
